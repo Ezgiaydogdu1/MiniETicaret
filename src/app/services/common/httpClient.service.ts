39963b746016d608bb9fc2ccb6dcf36f660f2e42
Ezgi Aydoğdu <ezgiaydogdu528@gmail.com>
2023-09-24 16:43:32 +0300

    httpClientServisi

--- src/app/services/common/httpClient.service.ts
+++ src/app/services/common/httpClient.service.ts
@@ -1,31 +1,32 @@
-// import { Inject, Injectable } from '@angular/core';
-// import {HttpClient} from '@angular/common/http'
+ import { Inject, Injectable } from '@angular/core';
+ import {HttpClient,HttpHeaders} from '@angular/common/http'
+import { Observable } from 'rxjs';
+ @Injectable({
+   providedIn: 'root'
+ })
+ export class HttpClientService {
+  constructor(private hhtpClient:HttpClient,@Inject("baseUrl") private baseUrl:string) { }
+   private url(requestParameter: Partial<RequestParameters>):string {
+    return `${requestParameter.baseUrl ? requestParameter.baseUrl : this.baseUrl}/${requestParameter.controller}${requestParameter.action ? `/${requestParameter.action}` : ""}` ;
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class HttpClientService {
-//   constructor(private hhtpClient:HttpClient,@Inject("baseUrl") private baseUrl:string) { }
-//   private url(requestParameter: Partial<RequestParameters>):string {
-//     return `${requestParameter.baseUrl ? requestParameter.baseUrl : this.baseUrl}/${requestParameter.controller}${requestParameter.action ? `/${requestParameter.action}` : ""}`;
+   }
+  get<T>(requestParameter: Partial<RequestParameters>, id?:string ):Observable<T> {
+    let url :string ="";
+    if (requestParameter.fullEndPoint)
+      url =requestParameter.fullEndPoint;
+     else
+     url =`${this.url(requestParameter)}${id ? `/${id}` : ""}`;
+     return this.hhtpClient.get<T>(url , {headers:requestParameter.headers})
+   }
+  post(){}  
+   put(){}
+  delete(){}
+ }
+ export class RequestParameters{
+   controller?:string;
+   action ?:string;
+   headers ?:  HttpHeaders;
+   baseUrl ?: string;
+   fullEndPoint ?:string;
 
-//   }
-//   get<T>(requestParameter: Partial<RequestParameters>, id?:string ){
-//     let url :string ="";
-//     if (requestParameter.fullEndPoint)
-//       url =requestParameter.fullEndPoint;
-//     else
-//     url =`${this.url(requestParameter)}`;
-//     this.hhtpClient.get()
-//   }
-//   post(){}
-//   put(){}
-//   delete(){}
-// }
-// export class RequestParameters{
-//   controller?:string;
-//   action ?:string;
-//   headers ?:  HttpHeaders;
-//   baseUrl ?: string;
-//   fullEndPoint ?: String
-// }
+ }
