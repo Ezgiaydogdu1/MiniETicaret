50415ce6b21d591e9a9e3737b90a4dc14704a431
Ezgi Aydoğdu <ezgiaydogdu528@gmail.com>
2023-09-27 23:54:00 +0300

    validation

--- src/app/admin/components/product/create/create.component.ts
+++ src/app/admin/components/product/create/create.component.ts
@@ -1,25 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { Create_Product } from 'src/app/contracts/create_product';
-import { ProductService } from 'src/app/services/common/models/product.service';
+import { EventEmitter } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from '../../../../base/base.component';
+import { Create_Product } from '../../../../contracts/create_product';
+import {AlertifyService,MessageType,Position} from '../../../../services/admin/alertify.service';
+import { ProductService } from '../../../../services/common/models/product.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
-export class CreateComponent implements OnInit {
-  constructor(private productService:ProductService){}
-  ngOnInit(): void {
-    
+export class CreateComponent extends BaseComponent implements OnInit {
+  constructor(
+    spiner: NgxSpinnerService,
+    private productService: ProductService,
+    private alertify: AlertifyService
+  ) {
+    super(spiner);
   }
-  create(name:HTMLInputElement,stock:HTMLInputElement,price:HTMLInputElement){
-    const create_product:Create_Product=new Create_Product();
-    create_product.name=name.value;
-    create_product.stock=parseInt(stock.value);
-    create_product.price=parseFloat(price.value);
-  
-  this.productService.Create(create_product)
-  }
-  
 
+  ngOnInit(): void {}
+
+  create(
+    name: HTMLInputElement,
+    stock: HTMLInputElement,
+    price: HTMLInputElement
+  ) {
+    this.showSpinner(SpinnerType.BallAtom);
+    const create_product: Create_Product = new Create_Product();
+    create_product.name = name.value;
+    create_product.stock = parseInt(stock.value);
+    create_product.price = parseFloat(price.value);
+
+    this.productService.Create(
+      create_product,
+      () => {
+        this.hideSpinner(SpinnerType.BallAtom);
+        this.alertify.message('Ürün başarıyla eklenmiştir.', {
+          dismissOthers: true,
+          messageType: MessageType.Success,
+          position: Position.TopRight,
+        });
+        
+      },
+      (errorMessage:string) => {
+        this.alertify.message(errorMessage, {
+          dismissOthers: true,
+          messageType: MessageType.Error,
+          position: Position.TopRight,
+        });
+      }
+    );
+  }
 }
--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -1,18 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClientService } from '../httpClient.service';
 import { Create_Product } from 'src/app/contracts/create_product';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
-  constructor(private httpClientService:HttpClientService) { }
-  Create(product:Create_Product){
-    this.httpClientService.post({
-      controller:"products"
-    },product).subscribe(result=> {
-      alert("işlem başarılı");
-    })
+  constructor(private httpClientService: HttpClientService) {}
+  Create(product: Create_Product, successCallBack?:any,errorCallBack?:any) {
+    this.httpClientService
+      .post(
+        {
+          controller: 'products',
+        },
+        product
+      )
+      .subscribe(
+        (result) => {
+        successCallBack();
+        },
+        (errorResponse: HttpErrorResponse) => {
+          const _error: Array<{ key: string; value: Array<string> }> =
+            errorResponse.error;
+          let message = '';
+          _error.forEach((v, index) => {
+            v.value.forEach((_v, _index) => {
+              message += `${_v}`;
+            });
+          });
+          errorCallBack(message);
+         
+        }
+      );
   }
 }
